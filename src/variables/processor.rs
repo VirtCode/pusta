31412de0bcbbed1dc7fffba79bcecce22e58b53b
Joshua <joshua.c.stalder@gmail.com>
2023-09-26 22:09:03 +0200

    feat: finished tokenizer

--- src/variables/processor.rs
+++ src/variables/processor.rs
@@ -1,39 +1,74 @@
 use std::ops::Range;
 use std::str::FromStr;
+use codespan_reporting::diagnostic::{Diagnostic, Label};
+use codespan_reporting::files::SimpleFiles;
+use codespan_reporting::term;
+use codespan_reporting::term::termcolor::{ColorChoice, StandardStream};
+use lazy_regex::{Lazy, lazy_regex, regex};
 use regex::{Regex, Replacer};
-use crate::variables::Variable;
 
-/*
-% pusta.hostname %
+/// Intermediate type for an error occurring when resolving variables.
+/// This type is intended to be converted to a codespan-reporting error.
+#[derive(Debug)]
+pub struct VariableError {
+    title: String,
+    primary: (Range<usize>, String),
+    secondary: Vec<(Range<usize>, String)>,
+    summary: String
+}
 
-% list test.thing %
+impl VariableError {
 
-% end %
+    /// Prints the error to stdout using codespan-reporting.
+    pub fn print(&self, filename: &str, content: &str) {
 
-% if amongus %
-% else %
-% end %
- */
+        let mut files = SimpleFiles::new();
+        let input = files.add(filename, content);
 
-pub const PATTERN_EXPRESSION: &str = r"%\s*([^%]+)\s*%";
-pub const PATTERN_TOKEN: &str = r"\s*([^\s]+)\s*";
+        let mut labels = vec![ Label::primary(input, self.primary.0.clone()).with_message(&self.primary.1)];
+        labels.append(&mut self.secondary.iter().map(|(range, s)| Label::secondary(input, range.clone()).with_message(s)).collect());
 
-pub const EXPRESSION_PATTERN: &str = r"%(?:\s*([^\s%]+)\s*)+%";
+        let diagnostic = Diagnostic::error()
+            .with_message(&self.title)
+            .with_labels(labels)
+            .with_notes(vec![ self.summary.clone() ]);
 
-pub const VARIABLE_PATTERN: &str = r"([\w.-]+)";
+        let writer = StandardStream::stderr(ColorChoice::Always);
+        let config = term::Config::default();
 
-pub const KEYWORD_LIST: &str = "list";
-pub const KEYWORD_IF: &str = "if";
-pub const KEYWORD_ELSE: &str = "else";
-pub const KEYWORD_END: &str = "end";
-pub const KEYWORD_EVAL: &str = "eval";
-pub const KEYWORD_INCLUDE: &str = "include";
+        term::emit(&mut writer.lock(), &config, &files, &diagnostic).unwrap(); // TODO: Handle this gracefully
+    }
+}
 
+/// Matches a keyword, e.g. !list
+static REGEX_KEYWORD: Lazy<Regex> = lazy_regex!(r"^\s*!([A-Za-z]+)\s+");
+
+/// Matches a boolean literal, e.g. true
+const REGEX_LITERAL_BOOLEAN: Lazy<Regex> = lazy_regex!(r"^\s*([A-Za-z]+)\s+");
+/// Matches a number literal, e.g. -1345.2768
+const REGEX_LITERAL_NUMBER: Lazy<Regex> = lazy_regex!(r"^\s*(-?[0-9]+(?:\.[0-9]+)?)\s+");
+/// Matches a string literal, e.g. "a string literal with inside \"quotes\" and newlines \n"
+const REGEX_LITERAL_STRING: Lazy<Regex> = lazy_regex!(r#"^\s*"(.*?[^\\])"\s+"#);
+
+/// Matches a variable name, e.g. theme.my-fancy-variable.color
+const REGEX_VARIABLE_NAME: Lazy<Regex> = lazy_regex!(r"^\s*([A-Za-z-]+(?:\.[A-Za-z-]+)*)(?:(?:\s+)|(?::))");
+/// Matches a variable modifier name, e.g. :my-awesome-modifier
+const REGEX_VARIABLE_MODIFIER_NAME: Lazy<Regex> = lazy_regex!(r"^:([A-Za-z-]+)");
+/// Matches an opening brace for a modifier, e.g. (
+const REGEX_VARIABLE_MODIFIER_OPEN: Lazy<Regex> = lazy_regex!(r"^\(\s*");
+/// Matches a closing brace for a modifier, e.g. )
+const REGEX_VARIABLE_MODIFIER_CLOSE: Lazy<Regex> = lazy_regex!(r"^\s*\)");
+
+
+/// Stores a token and where it's from
+#[derive(Debug)]
 pub struct Token {
     range: Range<usize>,
     token: TokenType
 }
 
+/// Stores a type of token, either a variable, literal or keyword
+#[derive(Debug)]
 pub enum TokenType {
     // pusta.hostname, theme.background:color-format( variable another.variable )
     Variable {
@@ -50,116 +85,66 @@ pub enum TokenType {
     }
 }
 
+/// Stores a literal, either a number, string or boolean
+#[derive(Debug)]
 pub enum TokenLiteral {
     String(String),
     Number(f64),
     Boolean(bool)
 }
 
-pub struct VariableError {
-    title: String,
-    primary: (Range<usize>, String),
-    secondary: Vec<(Range<usize>, String)>,
-    summary: String
-}
 
-pub fn process_variables(input: &str, variables: Variable, expression_regex: Option<String>) -> anyhow::Result<String> {
-    let token = Regex::new(PATTERN_TOKEN).expect("predefined regex pattern did not compile");
-    let variable = Regex::new(VARIABLE_PATTERN).expect("predefined regex pattern did not compile");
-
-    // possibly custom expression regex
-    let expression = if let Some(string) = expression_regex {
-        Regex::new(&string)?
-    } else {
-        Regex::new(PATTERN_EXPRESSION).expect("predefined regex pattern did not compile")
-    };
-
-    let mut result = String::new();
-
-    let mut last: usize = 0;
-    for expressions in expression.captures_iter(input) {
-        let sequence = expressions.get(0).unwrap();
-        let tokens = expressions.get(1).unwrap().as_str();
-
-        result += &input[last..sequence.start()];
-        last = sequence.end();
-
-        let variable = variables.find(token.captures(tokens).unwrap().get(1).unwrap().as_str()).unwrap();
-        if let Variable::Value(val) = variable {
-            result += val;
-        }
-    }
-    result += &input[last..];
-
-    Ok(result)
-}
-
-const PATTERN_KEYWORD: &str = r"!([A-Za-z]+\s+)";
-const PATTERN_LITERAL_BOOLEAN: &str = r"([A-Za-z]+)\s+";
-const PATTERN_LITERAL_NUMBER: &str = r"(-?[0-9]+(?:\.[0-9]+)?)\s+";
-const PATTERN_LITERAL_STRING: &str = r#""(.*[^\\])"\s+"#;
-const PATTERN_VARIABLE_NAME: &str = r"([A-Za-z-]+(?:\.[A-Za-z-]+)*)(?:(?:\s+)|(?::))";
-const PATTERN_VARIABLE_MODIFIER_NAME: &str = r":([A-Za-z-]+)";
-const PATTERN_VARIABLE_MODIFIER_OPEN: &str = r"\(\s*";
-
-pub fn read_token(input: &str, position: usize, keyword_allowed: bool) -> Result<Token, VariableError> {
-    let regex_keyword = Regex::new(PATTERN_KEYWORD).expect("failed to compile regex");
-    let regex_literal_boolean = Regex::new(PATTERN_LITERAL_BOOLEAN).expect("failed to compile regex");
-    let regex_literal_number = Regex::new(PATTERN_LITERAL_NUMBER).expect("failed to compile regex");
-    let regex_literal_string= Regex::new(PATTERN_LITERAL_STRING).expect("failed to compile regex");
-    let regex_variable_name = Regex::new(PATTERN_VARIABLE_NAME).expect("failed to compile regex");
-    let regex_variable_modifier_name = Regex::new(PATTERN_VARIABLE_MODIFIER_NAME).expect("failed to compile regex");
-    let regex_variable_modifier_open = Regex::new(PATTERN_VARIABLE_MODIFIER_OPEN).expect("failed to compile regex");
-
+/// Reads a keyword at the given position inside the input string
+pub fn read_token_at(input: &str, position: usize) -> Result<Token, VariableError> {
     // Keyword
-    if let Some(keyword) = regex_keyword.captures_at(input, position) {
+    if let Some(keyword) = REGEX_KEYWORD.captures(&input[position..]) {
         return Ok(Token {
-            range: keyword.get(0).expect("regex should have group").range(),
-            token: TokenType::Keyword { word: keyword.get(1).expect("regex should have group").into() }
+            range: shift_range(keyword.get(0).expect("regex should have group").range(), position),
+            token: TokenType::Keyword { word: keyword.get(1).expect("regex should have group").as_str().to_owned() }
         })
     }
 
     // Literals
-    if let Some(boolean) = regex_literal_boolean.captures_at(input, position) {
+    if let Some(boolean) = REGEX_LITERAL_BOOLEAN.captures(&input[position..]) {
         let text = boolean.get(1).expect("regex should have group").as_str();
 
         if text.eq_ignore_ascii_case("true") || text.eq_ignore_ascii_case("yes") {
             return Ok(Token {
-                range: boolean.get(0).expect("regex should have group").range(),
+                range: shift_range(boolean.get(0).expect("regex should have group").range(), position),
                 token: TokenType::Literal { value: TokenLiteral::Boolean(true)}
             })
         }
         if text.eq_ignore_ascii_case("false") || text.eq_ignore_ascii_case("no") {
             return Ok(Token {
-                range: boolean.get(0).expect("regex should have group").range(),
+                range: shift_range(boolean.get(0).expect("regex should have group").range(), position),
                 token: TokenType::Literal { value: TokenLiteral::Boolean(false)}
             })
         }
     }
 
-    if let Some(number) = regex_literal_number.captures_at(input, position) {
+    if let Some(number) = REGEX_LITERAL_NUMBER.captures(&input[position..]) {
         let m = number.get(1).expect("regex should have group");
 
         let num = f64::from_str(m.as_str()).map_err(|e| {
             VariableError {
                 title: "invalid number literal".into(),
-                primary: (m.range(), "is not a valid number".into()),
+                primary: (shift_range(m.range(), position), "is not a valid number".into()),
                 secondary: vec![],
                 summary: format!("´{}´ is not a valid number:\n  {e}", m.as_str())
             }
         })?;
 
         return Ok(Token {
-            range: number.get(0).expect("regex should have group").range(),
+            range: shift_range(number.get(0).expect("regex should have group").range(), position),
             token: TokenType::Literal { value: TokenLiteral::Number(num) }
         })
     }
 
-    if let Some(string) = regex_literal_string.captures_at(input, position) {
+    if let Some(string) = REGEX_LITERAL_STRING.captures(&input[position..]) {
         let s = string.get(1).expect("regex should have group").as_str();
 
         return Ok(Token {
-            range: string.get(0).expect("regex should have group").range(),
+            range: shift_range(string.get(0).expect("regex should have group").range(), position),
             token: TokenType::Literal { value: TokenLiteral::String(
                 s.replace("\\n", "\n").replace("\\\"", "\"")
             )}
@@ -167,32 +152,58 @@ pub fn read_token(input: &str, position: usize, keyword_allowed: bool) -> Result
     }
 
     // Variables
-    if let Some(m) = regex_variable_name.captures_at(input, position) {
+    if let Some(m) = REGEX_VARIABLE_NAME.captures(&input[position..]) {
         let name = m.get(1).expect("regex should have group");
 
-        let mut pos = name.end();
-        while let Some(modifier_capture) = regex_variable_modifier_name.captures_at(input, pos) {
+        // Read modifiers
+        let mut modifiers = vec![];
+        let mut pos = shift_range(name.range(), position).end;
+        while let Some(modifier_capture) = REGEX_VARIABLE_MODIFIER_NAME.captures(&input[pos..]) {
             let modifier_name = modifier_capture.get(1).expect("regex should have group");
+            pos = shift_range(modifier_name.range(), pos).end; // Preemptively set position of next token
 
+            // Read arguments for modifier when a brace opens
             let mut arguments = vec![];
-            if let Some(modifier_open_cap) = regex_variable_modifier_open.captures_at(input, modifier_name.end()) {
-                let mut token_pos = modifier_open_cap.get(0).expect("regex should have group").end();
+            if let Some(modifier_open_cap) = REGEX_VARIABLE_MODIFIER_OPEN.captures(&input[pos..]) {
 
-                // loop, try read closing brace, if not try read token
+                let mut token_pos = shift_range(modifier_open_cap.get(0).expect("regex should have group").range(),pos).end;
+                loop {
+                    // Break when having read closing brace
+                    if let Some(close_cap) = REGEX_VARIABLE_MODIFIER_CLOSE.captures(&input[token_pos..]) {
+                        pos = shift_range(close_cap.get(0).expect("regex should have group").range(), token_pos).end; // Position of next token is now after the brace
+                        break;
+                    }
 
+                    // Read token which is argument
+                    let mut token = read_token_at(input, token_pos)?;
+                    token_pos = token.range.end;
+                    arguments.push(token);
+                }
             }
 
-
-
+            modifiers.push((modifier_name.as_str().to_owned(), arguments));
         }
 
-
+        return Ok(Token {
+            range: (shift_range(m.get(0).expect("regex should have group").range(), position).start)..(pos),
+            token: TokenType::Variable { name: name.as_str().to_owned(), modifiers }
+        })
     }
 
-
-
-    Ok(Token::Keyword { word: "asdf".to_string() })
+    Err(VariableError {
+        title: "expected valid token".into(),
+        primary: (position..position, "no valid token found here".into()),
+        secondary: vec![],
+        summary: "did not encounter a valid token, expected either a literal, keyword or variable".into()
+    })
 }
 
+/// Shifts a range forward by a given position.
+/// This has to be used to shift ranges returned from capture groups of regexes, so they are valid in the whole string and not only the substring provided to the method.
+/// And yes, the captures_at method does not do the trick since anchors like ^ do not work there.
+fn shift_range(mut range: Range<usize>, position: usize) -> Range<usize> {
+    range.start += position;
+    range.end += position;
 
-
+    range
+}

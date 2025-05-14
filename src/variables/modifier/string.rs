95878818d482d8a1a32b5fddb26f2fcc4e037fd6
WhySoBad <49595640+WhySoBad@users.noreply.github.com>
2025-05-14 15:51:39 +0200

    feat: add more case modifiers (#6)
    * feat: add more case modifiers
    
    * fix: undo some formatting

--- src/variables/modifier/string.rs
+++ src/variables/modifier/string.rs
@@ -1,7 +1,9 @@
-use crate::variables::modifier::{Modifier, ModifierError};
 use crate::variables::modifier::ModifierErrorType::{ParameterAmount, ParameterType, VariableType};
+use crate::variables::modifier::{Modifier, ModifierError};
 use crate::variables::Value;
 use crate::variables::Value::{Boolean, String};
+
+use super::ModifierErrorNote;
 
 /// This modifier converts a string to upper case
 pub struct UpperCaseModifier;
@@ -13,10 +15,8 @@ impl Modifier for UpperCaseModifier {
 
         // Evaluate
         match variable {
-            String(s) => {
-                Ok(String(s.to_uppercase()))
-            }
-            _ => { Err(ModifierError::simple(VariableType(String("".into())))) }
+            String(s) => Ok(String(s.to_uppercase())),
+            _ => Err(ModifierError::simple(VariableType(String("".into())))),
         }
     }
 }
@@ -31,10 +31,155 @@ impl Modifier for LowerCaseModifier {
 
         // Evaluate
         match variable {
-            String(s) => {
-                Ok(String(s.to_lowercase()))
+            String(s) => Ok(String(s.to_lowercase())),
+            _ => Err(ModifierError::simple(VariableType(String("".into())))),
+        }
+    }
+}
+
+/// Turn a string into it's case segments based on an origin case
+///
+/// The segments are returned as a vector of lower-cased strings
+fn into_case_segments(
+    s: std::string::String,
+    case: &str,
+) -> Result<Vec<std::string::String>, std::string::String> {
+    let segments = match case {
+        KEBAB_CASE_MODIFIER => s.to_lowercase().split("-").map(std::string::String::from).collect(),
+        SNAKE_CASE_MODIFIER => s.to_lowercase().split("_").map(std::string::String::from).collect(),
+        CAMEL_CASE_MODIFIER | PASCAL_CASE_MODIFIER => {
+            let mut c = s.chars();
+            // uppercase first letter to transform camelCase to PascalCase
+            let s = match c.next() {
+                None => std::string::String::new(),
+                Some(f) => f.to_uppercase().chain(c).collect(),
+            };
+
+            let mut previous_upper = true;
+            let mut start = 0;
+            let mut segments = Vec::new();
+
+            if s.len() == 1 {
+                return Ok(vec![s.to_lowercase()]);
             }
-            _ => { Err(ModifierError::simple(VariableType(String("".into())))) }
+
+            s[1..].chars().enumerate().for_each(|(i, c)| {
+                if c.is_uppercase() {
+                    segments.push(s[start..i+1].to_lowercase());
+                    start = i + 1;
+                } else if c.is_lowercase() && previous_upper {
+                    previous_upper = false;
+                }
+                if i == s.len() - 2 {
+                    segments.push(s[start..i+2].to_lowercase())
+                }
+            });
+            segments
+        }
+        _ => Err(format!("case '{case}' is not a valid origin case"))?,
+    };
+    Ok(segments)
+}
+
+pub struct CamelCaseModifier;
+pub const CAMEL_CASE_MODIFIER: &str = "case-camel";
+impl Modifier for CamelCaseModifier {
+    fn evaluate(&self, variable: Value, parameters: Vec<Value>) -> Result<Value, ModifierError> {
+        if parameters.len() != 1 { Err(ModifierError::simple(ParameterAmount(1)))? }
+
+        match (variable, &parameters[0]) {
+            (String(s), String(c)) => {
+                let mut segments = into_case_segments(s, c.as_str()).map_err(|err| {
+                    ModifierError::noted(
+                        ParameterType(0, String("".into())),
+                        vec![ModifierErrorNote::Parameter(0, err)],
+                    )
+                })?.into_iter();
+                let first = segments.next().unwrap_or_default();
+                let rest = segments.map(|seg| {
+                    let mut c = seg.chars();
+                    match c.next() {
+                        None => std::string::String::new(),
+                        Some(f) => f.to_uppercase().chain(c).collect(),
+                    }
+                }).collect::<Vec<_>>().join("");
+                Ok(String(first + rest.as_str()))
+            }
+            (String(_), _) => Err(ModifierError::simple(ParameterType(0, String("".into())))),
+            _ => Err(ModifierError::simple(VariableType(String("".into())))),
+        }
+    }
+}
+
+pub struct SnakeCaseModifier;
+pub const SNAKE_CASE_MODIFIER: &str = "case-snake";
+impl Modifier for SnakeCaseModifier {
+    fn evaluate(&self, variable: Value, parameters: Vec<Value>) -> Result<Value, ModifierError> {
+        if parameters.len() != 1 { Err(ModifierError::simple(ParameterAmount(1)))? }
+
+        match (variable, &parameters[0]) {
+            (String(s), String(c)) => {
+                let segments = into_case_segments(s, c.as_str()).map_err(|err| {
+                    ModifierError::noted(
+                        ParameterType(0, String("".into())),
+                        vec![ModifierErrorNote::Parameter(0, err)],
+                    )
+                })?;
+                Ok(String(segments.join("_")))
+            }
+            (String(_), _) => Err(ModifierError::simple(ParameterType(0, String("".into())))),
+            _ => Err(ModifierError::simple(VariableType(String("".into())))),
+        }
+    }
+}
+
+pub struct PascalCaseModifier;
+pub const PASCAL_CASE_MODIFIER: &str = "case-pascal";
+impl Modifier for PascalCaseModifier {
+    fn evaluate(&self, variable: Value, parameters: Vec<Value>) -> Result<Value, ModifierError> {
+        if parameters.len() != 1 { Err(ModifierError::simple(ParameterAmount(1)))? }
+
+        match (variable, &parameters[0]) {
+            (String(s), String(c)) => {
+                let segments = into_case_segments(s, c.as_str()).map_err(|err| {
+                    ModifierError::noted(
+                        ParameterType(0, String("".into())),
+                        vec![ModifierErrorNote::Parameter(0, err)],
+                    )
+                })?;
+                let str = segments.iter().map(|seg| {
+                    let mut c = seg.chars();
+                    match c.next() {
+                        None => std::string::String::new(),
+                        Some(f) => f.to_uppercase().chain(c).collect(),
+                    }
+                }).collect::<Vec<_>>().join("");
+                Ok(String(str))
+            }
+            (String(_), _) => Err(ModifierError::simple(ParameterType(0, String("".into())))),
+            _ => Err(ModifierError::simple(VariableType(String("".into())))),
+        }
+    }
+}
+
+pub struct KebabCaseModifier;
+pub const KEBAB_CASE_MODIFIER: &str = "case-kebab";
+impl Modifier for KebabCaseModifier {
+    fn evaluate(&self, variable: Value, parameters: Vec<Value>) -> Result<Value, ModifierError> {
+        if parameters.len() != 1 { Err(ModifierError::simple(ParameterAmount(1)))? }
+
+        match (variable, &parameters[0]) {
+            (String(s), String(c)) => {
+                let segments = into_case_segments(s, c.as_str()).map_err(|err| {
+                    ModifierError::noted(
+                        ParameterType(0, String("".into())),
+                        vec![ModifierErrorNote::Parameter(0, err)],
+                    )
+                })?;
+                Ok(String(segments.join("-")))
+            }
+            (String(_), _) => Err(ModifierError::simple(ParameterType(0, String("".into())))),
+            _ => Err(ModifierError::simple(VariableType(String("".into())))),
         }
     }
 }
@@ -101,6 +246,82 @@ mod test {
     }
 
     #[test]
+    fn camel() {
+        let modifier = get_modifier("case-camel").unwrap();
+
+        assert_eq!(String("thisIsCamelCase".to_string()), modifier.evaluate(String("thisIsCamelCase".to_string()), vec![String("case-camel".to_string())]).unwrap());
+        assert_eq!(String("thisIsCamelCase".to_string()), modifier.evaluate(String("this_is_camel_case".to_string()), vec![String("case-snake".to_string())]).unwrap());
+        assert_eq!(String("thisIsCamelCase".to_string()), modifier.evaluate(String("ThisIsCamelCase".to_string()), vec![String("case-pascal".to_string())]).unwrap());
+        assert_eq!(String("thisIsCamelCase".to_string()), modifier.evaluate(String("this-is-camel-case".to_string()), vec![String("case-kebab".to_string())]).unwrap());
+
+        assert_eq!(String("p".to_string()), modifier.evaluate(String("P".to_string()), vec![String("case-pascal".to_string())]).unwrap());
+        assert_eq!(String("pP".to_string()), modifier.evaluate(String("PP".to_string()), vec![String("case-pascal".to_string())]).unwrap());
+
+        // require exactly one parameter
+        assert!(modifier.evaluate(String("thisIsCamelCase".to_string()), vec![]).is_err());
+        assert!(modifier.evaluate(String("thisIsCamelCase".to_string()), vec![String("".into()), String("".into())]).is_err());
+        // parameter needs to be a well-known case type
+        assert!(modifier.evaluate(String("thisIsCamelCase".to_string()), vec![String("".into())]).is_err());
+    }
+
+    #[test]
+    fn snake() {
+        let modifier = get_modifier("case-snake").unwrap();
+
+        assert_eq!(String("this_is_snake_case".to_string()), modifier.evaluate(String("thisIsSnakeCase".to_string()), vec![String("case-camel".to_string())]).unwrap());
+        assert_eq!(String("this_is_snake_case".to_string()), modifier.evaluate(String("this_is_snake_case".to_string()), vec![String("case-snake".to_string())]).unwrap());
+        assert_eq!(String("this_is_snake_case".to_string()), modifier.evaluate(String("ThisIsSnakeCase".to_string()), vec![String("case-pascal".to_string())]).unwrap());
+        assert_eq!(String("this_is_snake_case".to_string()), modifier.evaluate(String("this-is-snake-case".to_string()), vec![String("case-kebab".to_string())]).unwrap());
+
+        assert_eq!(String("p".to_string()), modifier.evaluate(String("P".to_string()), vec![String("case-pascal".to_string())]).unwrap());
+        assert_eq!(String("p_p".to_string()), modifier.evaluate(String("PP".to_string()), vec![String("case-pascal".to_string())]).unwrap());
+
+        // require exactly one parameter
+        assert!(modifier.evaluate(String("this_is_snake_case".to_string()), vec![]).is_err());
+        assert!(modifier.evaluate(String("this_is_snake_case".to_string()), vec![String("".into()), String("".into())]).is_err());
+        // parameter needs to be a well-known case type
+        assert!(modifier.evaluate(String("this_is_snake_case".to_string()), vec![String("".into())]).is_err());
+    }
+
+    #[test]
+    fn pascal() {
+        let modifier = get_modifier("case-pascal").unwrap();
+
+        assert_eq!(String("ThisIsPascalCase".to_string()), modifier.evaluate(String("thisIsPascalCase".to_string()), vec![String("case-camel".to_string())]).unwrap());
+        assert_eq!(String("ThisIsPascalCase".to_string()), modifier.evaluate(String("this_is_pascal_case".to_string()), vec![String("case-snake".to_string())]).unwrap());
+        assert_eq!(String("ThisIsPascalCase".to_string()), modifier.evaluate(String("ThisIsPascalCase".to_string()), vec![String("case-pascal".to_string())]).unwrap());
+        assert_eq!(String("ThisIsPascalCase".to_string()), modifier.evaluate(String("this-is-pascal-case".to_string()), vec![String("case-kebab".to_string())]).unwrap());
+
+        assert_eq!(String("P".to_string()), modifier.evaluate(String("p".to_string()), vec![String("case-camel".to_string())]).unwrap());
+        assert_eq!(String("PP".to_string()), modifier.evaluate(String("pP".to_string()), vec![String("case-camel".to_string())]).unwrap());
+
+        // require exactly one parameter
+        assert!(modifier.evaluate(String("ThisIsPascalCase".to_string()), vec![]).is_err());
+        assert!(modifier.evaluate(String("ThisIsPascalCase".to_string()), vec![String("".into()), String("".into())]).is_err());
+        // parameter needs to be a well-known case type
+        assert!(modifier.evaluate(String("ThisIsPascalCase".to_string()), vec![String("".into())]).is_err());
+    }
+
+    #[test]
+    fn kebab() {
+        let modifier = get_modifier("case-kebab").unwrap();
+
+        assert_eq!(String("this-is-kebab-case".to_string()), modifier.evaluate(String("thisIsKebabCase".to_string()), vec![String("case-camel".to_string())]).unwrap());
+        assert_eq!(String("this-is-kebab-case".to_string()), modifier.evaluate(String("this_is_kebab_case".to_string()), vec![String("case-snake".to_string())]).unwrap());
+        assert_eq!(String("this-is-kebab-case".to_string()), modifier.evaluate(String("ThisIsKebabCase".to_string()), vec![String("case-pascal".to_string())]).unwrap());
+        assert_eq!(String("this-is-kebab-case".to_string()), modifier.evaluate(String("this-is-kebab-case".to_string()), vec![String("case-kebab".to_string())]).unwrap());
+
+        assert_eq!(String("p".to_string()), modifier.evaluate(String("p".to_string()), vec![String("case-camel".to_string())]).unwrap());
+        assert_eq!(String("p-p".to_string()), modifier.evaluate(String("pP".to_string()), vec![String("case-camel".to_string())]).unwrap());
+
+        // require exactly one parameter
+        assert!(modifier.evaluate(String("this-is-kebab-case".to_string()), vec![]).is_err());
+        assert!(modifier.evaluate(String("this-is-kebab-case".to_string()), vec![String("".into()), String("".into())]).is_err());
+        // parameter needs to be a well-known case type
+        assert!(modifier.evaluate(String("this-is-kebab-case".to_string()), vec![String("".into())]).is_err());
+    }
+
+    #[test]
     fn contains() {
         let modifier = get_modifier("contains").unwrap();
 
@@ -116,7 +337,6 @@ mod test {
 
     #[test]
     fn tilde() {
-
         let modifier = get_modifier("tilde").unwrap();
 
         // valid
@@ -127,5 +347,4 @@ mod test {
         assert!(matches!(modifier.evaluate(Boolean(false), vec![]), Err(_)));
         assert!(matches!(modifier.evaluate(String("asdf".to_string()), vec![Boolean(true)]), Err(_)));
     }
-
 }
